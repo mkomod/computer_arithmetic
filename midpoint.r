b7c711160ce75ae436573cd5899d77ac19cf2c65
mkomod <mkomodromos01@gmail.com>
2019-11-08 15:46:04 +0000

    midpoint

---
+++ midpoint.r
@@ -0,0 +1,32 @@
+
+
+midpoint <- function(func, a, b, n=1000) {
+    # Midpoint algorithm for determining the integral of a
+    # 1 - Dimensional function
+    #
+    # Args:
+    #   func: (function) the function you want to integrate
+    #   a, b: (float) the start and endpoints of the itegral
+    #   n: (int) the number of midpoints to use
+    #
+    # Returns:
+    #   (float) the integral of the funciton.
+    #
+    if (a >= b) {
+	stop("starting value larger than end")
+    } 
+    if (!all(is.finite(c(a, b, n)))) {
+	stop("Please provide finite values for a, b, n")
+    }
+    if (n < 1) {
+	stop("Please provide a valid number of midpoints")
+    }
+    dif <- 1/(2*n)
+    value <- mean(func(seq(a + dif, b - dif, by=1/n)))
+
+    # TODO: finish error implementation
+    # error <- (optim(par=a, fn=func, method="Brent", lower=a, upper=b, hessian=TRUE))$hessian / (24 * n ^ 2) * (b -a)^2
+    list(value = value)
+}
+
+
